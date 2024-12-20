de19771f91e9630b8c0f9195a57f28f741d21a0a
Modise75Morena <kamzamodise16@gmail.com>
2024-12-21 01:37:46 +0200

    add new project

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,17 @@
+number1 = 10
+number2= 5
+
+# addition
+
+k = number1 + number2
+print(f"ADDITION OF {number1} and {number2} is {k}")
+
+# subtraction
+
+a = number1 - number2
+print(f"Subtraction of {number1} and {number2} is {a}")
+
+# product
+
+m = number1 * number2
+print(f"Multiplication of {number1} and {number2} is {m}")
